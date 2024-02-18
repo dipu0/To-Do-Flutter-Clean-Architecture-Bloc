43db678e9bae7a37c62c25e9ef7bd62735ed0101
dipu0 <c.dipu0@gmail.com>
2024-02-19 00:25:11 +0600

    Initial commit

---
+++ lib/core/data/http/urls/api_urls.dart
@@ -0,0 +1,28 @@
+import '../../../../app/app.dart';
+
+part 'authentication_api_urls.dart';
+part 'dashboard_api_urls.dart';
+part 'trade_api_urls.dart';
+
+class ApiUrl implements AuthenticationApiUrls, DashboardApiUrls{
+  var baseUrl = "${appConfig.getApiClientConfig().baseUrl}api/";
+  var apiVersion = appConfig.getApiClientConfig().apiVersion;
+
+  @override
+  String get emailLoginUrl => "${baseUrl}token/";
+
+  @override
+  String get facebookLoginUrl => throw UnimplementedError();
+
+  @override
+  String get gmailLoginUrl => throw UnimplementedError();
+
+  @override
+  String get registrationUrl => throw UnimplementedError();
+
+  // @override
+  // String get getAllTrade => "${baseUrl}Item/getall";
+
+  @override
+  String get getDashboardData => throw UnimplementedError();
+}
